e42d8b350618d3ef8b8b5e658ef4be365c53b1bb
Jeff Swisher <jefftswisher@gmail.com>
2020-01-24 02:06:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ class App < Sinatra::Base
   # Define your GET '/' route below and respond with "Hello, World!"
 
   get '/' do 
-     resp.write "Hello, World!"
-     resp.status = 200
+     "Hello, World!"
    end 
   
 
